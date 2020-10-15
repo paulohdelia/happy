f8e96f2288bbca8bd8cb361ffc724643095acc8d
paulohdelia <paulohdelia@gmail.com>
2020-10-15 16:22:05 -0300

    add orphanage page

--- web/src/routes/index.tsx
+++ web/src/routes/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import CreateOrphanage from '../pages/CreateOrphanage';
 
 import Landing from '../pages/Landing';
+import Orphanage from '../pages/Orphanage';
 import OrphanagesMap from '../pages/OrphanagesMap';
 
 const Routes: React.FC = () => {
@@ -11,6 +13,8 @@ const Routes: React.FC = () => {
       <Switch>
         <Route exact path="/" component={Landing} />
         <Route exact path="/app" component={OrphanagesMap} />
+        <Route exact path="/orphanages/create" component={CreateOrphanage} />
+        <Route exact path="/orphanages/:id" component={Orphanage} />
       </Switch>
     </BrowserRouter>
   );
