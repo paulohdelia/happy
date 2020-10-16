c4b491668d9481346c12667e569e593fbfd64479
paulohdelia <paulohdelia@gmail.com>
2020-10-16 00:21:04 -0300

    init mobile project

---
+++ mobile/App.tsx
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const App: React.FC = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Hello World!</Text>
+      {/* eslint-disable-next-line react/style-prop-object */}
+      <StatusBar style="dark" />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default App;
